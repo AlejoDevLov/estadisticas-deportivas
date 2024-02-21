eeb7722d94a21c8460fd00d3540d0e5473f5b517
Alejandro Rios <alejo.950723@gmail.com>
2024-02-21 00:53:02 -0500

    archivo de config tailwind modificado

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,6 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  mode: 'jit',
-  purge: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
   theme: {
     extend: {
     },
